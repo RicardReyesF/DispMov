211f6b62d370180ca64c8a3010040c3ecb2baa08
unknown <rreyesfdz96@gmail.com>
2020-03-03 00:13:46 -0600

    paso 2

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         conexion conn = new conexion();
+         conexion conn = new conexion(this,"bd_prueba",null ,1);
     }
 }
